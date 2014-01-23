5b25184af953578f7a6d22e005f9c4d2f1dbaef2
Dmitry Tokmakov <dm.tokmakov@gmail.com>
2014-01-23 16:45:21 -0500

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * SelectBox box menu walker
+ * @author  : Dmitry Tokmakov
+ * @version : 1.0
+ * 
+ */
+
+class SelectBox_Menu_Walker extends Walker_Nav_Menu {
+        
+        function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+                global $wp_query;
+                $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+                
+                $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+                $selected = in_array('current-menu-item',$classes) ? 'selected="selected"' : '';
+                $output .= '<option '.$selected.' value="'.$item->url.'">';
+                $output .= $item->title;
+        }
+        function end_el( &$output, $item, $depth = 0, $args = array() ) {
+                $output .= "</option>";
+        }
+}
+?>
